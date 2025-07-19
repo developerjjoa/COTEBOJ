02bd6c7cb43606f60c267c7e5c23d144c05145a7
developerjjoa <developerjjoa@gmail.com>
2025-07-19 20:28:30 +0900

    BOJ_1008 나의 풀이

---
+++ src/class1/essential/BOJ1008/BOJ_1008_F.java
@@ -0,0 +1,13 @@
+package class1.essential.BOJ1008;
+
+import java.util.Scanner;
+
+public class BOJ_1008_F {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println((double) num1 / num2);
+    }
+}

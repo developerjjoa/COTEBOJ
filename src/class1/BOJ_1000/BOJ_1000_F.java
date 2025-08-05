b247b38913ed7c12cc194790b236ca7895665f89
developerjjoa <developerjjoa@gmail.com>
2025-08-06 00:56:20 +0900

    BOJ 1000번 - Scanner 사용 O(1)

---
+++ src/class1/BOJ_1000/BOJ_1000_F.java
@@ -0,0 +1,12 @@
+package class1.BOJ_1000;
+
+import java.util.Scanner;
+
+public class BOJ_1000_F {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+    }
+}

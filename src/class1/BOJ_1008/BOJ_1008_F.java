666b0dcc9f0c8c70b26c9bf02dde6989bc082998
developerjjoa <developerjjoa@gmail.com>
2025-08-06 02:05:09 +0900

    BOJ 1008번 - BufferedReader + StringTokenizer, 그냥 나누기만 하면 문제가 될 수도 있음 O(1)

---
+++ src/class1/BOJ_1008/BOJ_1008_F.java
@@ -0,0 +1,16 @@
+package class1.BOJ_1008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1008_F {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        System.out.println(A / B);
+    }
+}
